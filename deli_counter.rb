c7f49594f39fe84603842a496aee064e81580f25
Jazmin M <jazminmerckel@gmail.com>
2019-10-01 18:38:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,15 @@
 # Write your code here.
 
-def take_a_number(katz_deli,)
+def take_a_number(katz_deli, name)
+  
+end
 
-def line 
+def line(katz_deli)
  if 
    puts "The line is currently empty."
+end
+
+def now_serving(katz_deli)
+  if
+    puts "There is nobody waiting to be served!"
 end
