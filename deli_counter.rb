68b8a43ea357536e3862808a44cb539168c46632
Jazmin M <jazminmerckel@gmail.com>
2019-10-01 19:12:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,16 @@
 
 katz_deli = []
 
-
 def take_a_number(queue, name)
   queue << name
   puts "Welcome, #{name}. You are number #{queue.length} in line."
 end
 
-
+def line(queue)
+  if queue.length == 0
+    puts "The line is currently: "
+    puts "The line is currently empty."
+end
 
 def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
